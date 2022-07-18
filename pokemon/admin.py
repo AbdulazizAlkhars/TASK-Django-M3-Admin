ef4b9ee150e672729bf1905590d1e6f60b8a7184
AbdulazizAlkhars <alkharswork@gmail.com>
2022-07-18 20:37:55 +0300

    DONE

--- pokemon/admin.py
+++ pokemon/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Pokemon
+
+
+class PokemonAdmin (admin.ModelAdmin):
+    list_display = ('id', 'name', 'hp', 'active')
+    list_filter = ('active',)
+    fieldsets = (
+        (None, {
+            'fields': ('name', 'pokemon_type', 'hp', 'active')
+        }),
+        ('Localizations', {
+            'classes': ('collapse',),
+            'fields': ('name_fr', 'name_ar', 'name_jp'),
+        }),
+    )
+
 
 # Register your models here.
+admin.site.register(Pokemon, PokemonAdmin)
--- pokemon/models.py
+++ pokemon/models.py
@@ -1,3 +1,33 @@
+from random import choices
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
+
+
+class Pokemon(models.Model):
+    class PokemonType(models.TextChoices):
+        WATER = 'WA', ('WATER')
+        GRASS = 'GR', ('GRASS')
+        GHOST = 'GH', ('GHOST')
+        STEEL = 'ST', ('STEEL')
+        FAIRY = 'FA', ('FAIRY')
+
+    def is_upperclass(self):
+        return self.pokemon_type in {
+            self.PokemonType.JUNIOR,
+            self.PokemonType.SENIOR,
+        }
+    name = models.CharField(max_length=30)
+    pokemon_type = models.CharField(
+        max_length=2,
+        choices=PokemonType.choices,
+        default=PokemonType.WATER,
+    )
+    hp = models.PositiveIntegerField(default=0)
+    active = models.BooleanField(default=True)
+    name_fr = models.CharField(max_length=30, blank=True, null=True)
+    name_ar = models.CharField(max_length=30, blank=True, null=True)
+    name_jp = models.CharField(max_length=30, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
